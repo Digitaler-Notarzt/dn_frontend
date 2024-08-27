cad1abd8a499cccad8b6166f63781f2b3094da9a
bprinc <bprinc@student.tgm.ac.at>
2024-08-27 10:55:26 +0200

    Voice messages are visible in the chat together with their record duration

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -16,6 +16,7 @@ class _ChatScreenState extends State<ChatScreen> {
   late MicrophoneHelper _microphoneHelper;
   bool isKeyboardVisibl = false;
   bool _isRecording = false;
+  int startTime = 0;
 
   @override
   void initState() {
@@ -25,8 +26,35 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void _toggleRecording() async {
     await _microphoneHelper.toggleRecording();
+    if (!_microphoneHelper.isRecording) {
+      int duration = DateTime.timestamp().millisecondsSinceEpoch - startTime;
+      _sendAudioMessage(_microphoneHelper.recordingPath!, duration);
+    }
     setState(() {
       _isRecording = _microphoneHelper.isRecording;
+      startTime = DateTime.timestamp().millisecondsSinceEpoch;
+    });
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  void _sendAudioMessage(String audioFilePath, int durationMillis) {
+    Duration duration = Duration(milliseconds: durationMillis);
+    String formattedDuration = _formatDuration(duration);
+
+    setState(() {
+      messages.add(Message(audioFilePath: audioFilePath, audioDuration: formattedDuration, isUserMessage: true));
+      _scrollToBottom();
+    });
+
+    messages.add(Message(text: 'Nachricht erhalten', isUserMessage: false));
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
     });
   }
 
@@ -80,7 +108,12 @@ class _ChatScreenState extends State<ChatScreen> {
                   reverse: false,
                   itemCount: messages.length,
                   itemBuilder: (context, index) {
-                    return _buildMessageBubble(messages[index]);
+                    final message = messages[index];
+                    if (message.isAudioMessage) {
+                      return _buildAudioMessageBubble(message);
+                    } else {
+                      return _buildTextMessageBubble(message);
+                    }
                   },
                 ),
               ),
@@ -90,7 +123,7 @@ class _ChatScreenState extends State<ChatScreen> {
         ));
   }
 
-  Widget _buildMessageBubble(Message message) {
+  Widget _buildTextMessageBubble(Message message) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Align(
@@ -117,13 +150,62 @@ class _ChatScreenState extends State<ChatScreen> {
               ),
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
               child: Text(
-                message.text,
+                message.text!,
                 style: const TextStyle(color: Colors.white),
               ),
             ),
           ],
         ),
-        // child:
+      ),
+    );
+  }
+
+  Widget _buildAudioMessageBubble(Message message) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Align(
+        alignment: message.isUserMessage
+            ? Alignment.centerRight
+            : Alignment.centerLeft,
+        child: Column(
+          crossAxisAlignment: message.isAudioMessage
+              ? CrossAxisAlignment.end
+              : CrossAxisAlignment.start,
+          children: [
+            Text(
+              message.isUserMessage ? 'Ich' : 'Digitaler Notarzt',
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.black54),
+            ),
+            const SizedBox(height: 4),
+            Container(
+              constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.65),
+              decoration: BoxDecoration(
+                color: message.isUserMessage ? Colors.blueAccent : Colors.grey,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.audio_file_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    'Audio ${message.audioDuration}',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
